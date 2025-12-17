a9e36fe3a9d9c4255442f2abf6c2df4abfe78df0
Bedirhan <bedirah.kartal5@gmail.com>
2025-12-17 17:08:34 +0300

    Add Vercel Web Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import TurkishInsuranceLanding from "./App";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <TurkishInsuranceLanding />
+    <Analytics />
   </React.StrictMode>
 );
 
